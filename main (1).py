c7f0183c228cebdbf65e5e260df52b410a153ab4
jyotshna-24 <83664630+jyotshna-24@users.noreply.github.com>
2021-05-07 20:34:56 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,20 @@
+#x=int(2021)
+#y=int(input("enter your birth of year: "))
+#sum=x-y
+#print('your age is',sum,'.')
+
+a,b=5,2
+
+print("a+b={}".format(a+b))
+
+print("a-b={}".format(a-b))
+
+print("a*b={}".format(a*b))
+
+print("a/b={}".format(a/b))
+
+print("a%b={}".format(a%b))
+
+print("a//b={}".format(a//b))
+
+print("a**b={}".format(a**b))
